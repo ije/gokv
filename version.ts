2cc344829adf13fe5307caeb5658231a8ff8b962
X <i@jex.me>
2022-01-14 01:43:06 +0800

    Update version script

---
+++ version.ts
@@ -0,0 +1,40 @@
+/** `VERSION` managed by https://deno.land/x/publish */
+export const VERSION = "0.0.7"
+
+/** `prepublish` will be invoked before publish */
+export async function prepublish(version: string) {
+  for (const filename of [
+    "package.json",
+    "./docs/get-started.md",
+    "./examples/deno-hello-server/mod.ts",
+    "./examples/deno-session/mod.ts",
+  ]) {
+    await replaceVersion(filename, version)
+  }
+  await Deno.run({
+    cmd: ['npm', 'publish'],
+    stdin: 'inherit',
+    stdout: 'inherit',
+    stderr: 'inherit',
+  }).status()
+}
+
+/** `prepublish` will be invoked after publish */
+export async function postpublish(version: string) {
+  console.log("Upgraded to", version)
+}
+
+async function replaceVersion(filename: string, version: string) {
+  const readme = await Deno.readTextFile(filename)
+
+  if (filename === "package.json") {
+    const json = JSON.parse(readme)
+    json.version = version
+    await Deno.writeTextFile(filename, JSON.stringify(json, null, 2))
+  } else {
+    await Deno.writeTextFile(filename, readme.replace(
+      /\/\/deno\.land\/x\/gokv@[\d\.]+\//,
+      `//deno.land/x/gokv@${version}/`
+    ))
+  }
+}
