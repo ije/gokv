ab1db4d5a6aff2588c2a2df1867b639c8af9f8f7
X <i@jex.me>
2022-01-13 23:41:02 +0800

    Update node entry

--- node.ts
+++ node.ts
@@ -1,8 +1,13 @@
 import { Crypto } from "@peculiar/webcrypto"
-import nodeFetch from "node-fetch"
+import nodeFetch, { Request, Response, Headers } from "node-fetch"
+import { v4 as uuidv4 } from 'uuid'
 import mod from "./mod.ts"
 
 global.fetch = global.fetch || nodeFetch
+global.Headers = global.Headers || Headers
+global.Request = global.Request || Request
+global.Response = global.Response || Response
 global.crypto = global.crypto || new Crypto()
+global.crypto.randomUUID = global.crypto.randomUUID || (() => uuidv4())
 
 export default mod
