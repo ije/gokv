3de08f33b32855a79228b1a08cd60ccde708084b
X <i@jex.me>
2022-09-23 19:15:31 +0800

    Update version script

--- version.ts
+++ version.ts
@@ -3,36 +3,14 @@ export const VERSION = "0.0.12"
 
 /** `prepublish` will be invoked before publish */
 export async function prepublish(version: string) {
-  for (const filename of [
-    "package.json",
-    "./examples/deno-hello-server/mod.ts",
-    "./examples/deno-session/mod.ts",
-  ]) {
-    await replaceVersion(filename, version)
-  }
+  const text = await Deno.readTextFile("package.json")
+  const json = JSON.parse(text)
+  json.version = version
+  await Deno.writeTextFile("package.json", JSON.stringify(json, null, 2))
   await Deno.run({
     cmd: ['npm', 'publish'],
     stdin: 'inherit',
     stdout: 'inherit',
     stderr: 'inherit',
   }).status()
-}
-
-/** `prepublish` will be invoked after publish */
-export async function postpublish(version: string) {
-  console.log("Upgraded to", version)
-}
-
-async function replaceVersion(filename: string, version: string) {
-  const text = await Deno.readTextFile(filename)
-  if (filename === "package.json") {
-    const json = JSON.parse(text)
-    json.version = version
-    await Deno.writeTextFile(filename, JSON.stringify(json, null, 2))
-  } else {
-    await Deno.writeTextFile(filename, text.replace(
-      /(\/\/deno\.land\/x\/gokv@)[\d\.]+\//g,
-      `$1${version}/`
-    ))
-  }
 }
