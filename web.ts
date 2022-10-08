544fdb3b3ab0fc8649296199b980ff5fcacc7141
X <i@jex.me>
2022-10-09 04:34:52 +0800

    0.0.13

--- web.ts
+++ web.ts
@@ -8,7 +8,7 @@ import type {
   Uploader,
   UploaderOptions,
 } from "./types/web.d.ts";
-import atm from "./src/AccessTokenManager.ts";
+import atm from "./src/common/AccessTokenManager.ts";
 import UploaderImpl from "./src/Uploader.ts";
 import ChatRoomImpl from "./src/ChatRoom.ts";
 import CoEditImpl from "./src/CoEdit.ts";
