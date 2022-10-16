335608d9329836b97952b1d16e8441aa86eef065
X <i@jex.me>
2022-10-16 14:34:01 +0800

    Add websocet polyfill for nodejs

--- web-polyfill.mjs
+++ web-polyfill.mjs
@@ -1,10 +1,14 @@
 import { Crypto } from "@peculiar/webcrypto";
-import nodeFetch, { Headers, Request, Response } from "node-fetch";
-import { v4 as uuidv4 } from "uuid";
+import nodeFetch, { File, FormData, Headers, Request, Response } from "node-fetch";
+import { v4 as uuid } from "uuid";
+import ws from "websocket";
 
 global.fetch = global.fetch || nodeFetch;
+global.File = global.File || File;
+global.FormData = global.FormData || FormData;
 global.Headers = global.Headers || Headers;
 global.Request = global.Request || Request;
 global.Response = global.Response || Response;
 global.crypto = global.crypto || new Crypto();
-global.crypto.randomUUID = global.crypto.randomUUID || uuidv4;
+global.crypto.randomUUID = global.crypto.randomUUID || uuid;
+global.WebSocket = global.WebSocket || ws.w3cwebsocket;
