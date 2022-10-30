2160d35ecd7845937597849d68402f49d6da700a
X <i@jex.me>
2022-10-30 22:23:17 +0800

    0.0.16

--- version.ts
+++ version.ts
@@ -1,5 +1,5 @@
 /** `VERSION` managed by https://deno.land/x/publish */
-export const VERSION = "0.0.15";
+export const VERSION = "0.0.16";
 
 /** `prepublish` will be invoked before publish */
 export async function prepublish(version: string) {
