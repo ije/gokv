352b2cd0a32be9e7a66597fbd6d77c0105b63468
X <i@jex.me>
2022-11-07 15:47:15 +0800

    Update integration testing in nodejs

--- test/integration-test-node.mjs
+++ test/integration-test-node.mjs
@@ -64,7 +64,9 @@ await test("Storage", async () => {
   };
 
   // put key-value pairs
-  await Promise.all(Object.entries(records).map(([key, value]) => kv.put(key, value)));
+  await Promise.all(
+    Object.entries(records).map(([key, value]) => kv.put(key, value)),
+  );
   assert.deepEqual(await kv.list(), new Map(Object.entries(records)));
 
   // delete key-value pairs
@@ -74,28 +76,62 @@ await test("Storage", async () => {
   // put key-value pairs
   await kv.put(records);
   assert.deepEqual(await kv.list(), new Map(Object.entries(records)));
-  assert.deepEqual(await kv.get(["foo", "num"]), new Map([["foo", "bar"], ["num", 123]]));
+  assert.deepEqual(
+    await kv.get(["foo", "num"]),
+    new Map([["foo", "bar"], ["num", 123]]),
+  );
 
   // flush
   await kv.deleteAll();
   assert.deepEqual(await kv.list(), new Map());
 
   // list with condition
-  await kv.put(Object.fromEntries(new Array(10).fill(null).map((val, index) => [`k-${index}`, val])));
+  await kv.put(
+    Object.fromEntries(
+      new Array(10).fill(null).map((_, index) => [`k-${index}`, index]),
+    ),
+  );
   assert.deepEqual(
     await kv.list({ prefix: "k-" }),
-    new Map(new Array(10).fill(0).map((_, index) => [`k-${index}`, null])),
+    new Map(new Array(10).fill(null).map((_, index) => [`k-${index}`, index])),
   );
-  assert.deepEqual(await kv.list({ limit: 5 }), new Map(new Array(5).fill(0).map((_, index) => [`k-${index}`, null])));
-  assert.deepEqual(await kv.list({ limit: 3, reverse: true }), new Map([["k-7", null], ["k-8", null], ["k-9", null]]));
-  assert.deepEqual(await kv.list({ start: "k-7" }), new Map([["k-7", null], ["k-8", null], ["k-9", null]]));
-  assert.deepEqual(await kv.list({ start: "k-7", limit: 2 }), new Map([["k-7", null], ["k-8", null]]));
-  assert.deepEqual(await kv.list({ start: "k-7", end: "k-9" }), new Map([["k-7", null], ["k-8", null]]));
+  assert.deepEqual(
+    await kv.list({ limit: 5 }),
+    new Map(new Array(5).fill(null).map((_, index) => [`k-${index}`, index])),
+  );
+  assert.deepEqual(
+    await kv.list({ limit: 3, reverse: true }),
+    new Map([["k-7", 7], ["k-8", 8], ["k-9", 9]]),
+  );
+  assert.deepEqual(
+    await kv.list({ start: "k-7" }),
+    new Map([["k-7", 7], ["k-8", 8], ["k-9", 9]]),
+  );
+  assert.deepEqual(
+    await kv.list({ start: "k-7", limit: 2 }),
+    new Map([["k-7", 7], ["k-8", 8]]),
+  );
+  assert.deepEqual(
+    await kv.list({ start: "k-7", end: "k-9" }),
+    new Map([["k-7", 7], ["k-8", 8]]),
+  );
+
+  // sum
+  assert.deepEqual(
+    await kv.sum({ prefix: "k-" }),
+    {
+      items: 10,
+      sum: new Array(10).fill(null).reduce((acc, _, index) => acc + index, 0),
+    },
+  );
 
   // delete with condition
-  assert.deepEqual(await kv.delete({ limit: 5, reverse: true }), 5);
-  assert.deepEqual(await kv.delete(["k-3", "k-4"]), 2);
-  assert.deepEqual(await kv.list(), new Map(new Array(3).fill(0).map((_, index) => [`k-${index}`, null])));
+  assert.equal(await kv.delete({ limit: 5, reverse: true }), 5);
+  assert.equal(await kv.delete(["k-3", "k-4"]), 2);
+  assert.deepEqual(
+    await kv.list(),
+    new Map(new Array(3).fill(0).map((_, index) => [`k-${index}`, index])),
+  );
 });
 
 await test("Session Storage", async () => {
