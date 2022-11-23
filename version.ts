82e24a8ebba0a01eac52a91501bbadfe86850178
X <i@jex.me>
2022-11-23 19:49:27 +0800

    Update version script

--- version.ts
+++ version.ts
@@ -7,33 +7,22 @@ export async function prepublish(version: string) {
   await Deno.writeTextFile("package.json", JSON.stringify({ ...packageInfo, version }, null, 2));
   await run(Deno.execPath(), "fmt");
   await run("npm", "publish");
-  await updateVersion(version);
-}
-
-async function updateVersion(version: string) {
-  const files = await readDir("examples");
-  for (const file of files) {
-    if (file.endsWith(".ts")) {
-      const content = await Deno.readTextFile(file);
-      await Deno.writeTextFile(file, content.replace(/gokv@[\d\.]+/g, `gokv@${version}`));
-    }
-  }
-}
-
-async function readDir(root: string): Promise<string[]> {
-  const files: string[] = [];
-  for await (const dirEntry of Deno.readDir(root)) {
-    const path = `${root}/${dirEntry.name}`;
-    if (dirEntry.isDirectory) {
-      files.push(...(await readDir(path)));
-    } else {
-      files.push(path);
-    }
-  }
-  return files;
+  await updateVersion("examples", version);
 }
 
 async function run(cmd: string, ...args: string[]) {
   const p = Deno.run({ cmd: [cmd, ...args], stdin: "inherit", stdout: "inherit", stderr: "inherit" });
   await p.status();
+}
+
+async function updateVersion(dir: string, version: string, exts = [".ts"]) {
+  for await (const file of Deno.readDir(dir)) {
+    const path = `${dir}/${file.name}`;
+    if (file.isDirectory) {
+      await updateVersion(path, version, exts);
+    } else if (exts.includes(file.name.slice(file.name.lastIndexOf(".")))) {
+      const content = await Deno.readTextFile(path);
+      await Deno.writeTextFile(path, content.replace(/gokv@[\d\.]+/g, `gokv@${version}`));
+    }
+  }
 }
