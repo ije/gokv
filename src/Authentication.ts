9b9c698ec99a4510de5edaa37b40844b916512c5
X <i@jex.me>
2023-02-21 00:49:30 +0800

    Update login page style

--- src/Authentication.ts
+++ src/Authentication.ts
@@ -1,7 +1,7 @@
 import type { Authentication, AuthenticationOptions, AuthUser, LoginPageProps, Permission } from "../types/mod.d.ts";
-import SeesionImpl from "./Session.ts";
 import atm from "./AccessTokenManager.ts";
 import { providers } from "./AuthProviders.ts";
+import SeesionImpl from "./Session.ts";
 
 const DefaultLoginPage = (options: LoginPageProps) => {
   const LoginLink = (provider: string) => {
@@ -23,7 +23,7 @@ const DefaultLoginPage = (options: LoginPageProps) => {
     <title>${["Login", options.appName].filter(Boolean).join(" - ")}</title>
   </head>
   <style>
-    *{padding:0;margin:0;font:inherit}body{display:flex;height:100vh;font-family:Inter,sans-serif;overflow:hidden}header{display:flex;flex-direction:column;align-items:flex-end;justify-content:center;width:40vw;height:100vh;padding-right:2.4rem}header svg,header img{width:auto;height:2rem}header h1{font-size:1.6rem;font-weight:600;line-height:2;color:#24292e}header p{font-size:1rem;color:#586069;max-width:20rem;text-align:right}.links{display:flex;flex-direction:column;justify-content:center;align-items:flex-start;width:60vw;height:100vh;padding-left:2.4rem;border-left:1px solid #e9e9e9;background-color:#f9f9f9}a{display:flex;align-items:center;gap:.6rem;padding:.5rem 1.2rem;border:1px solid #e1e4e8;border-radius:.5rem;font-size:16px;line-height:1;color:#333;background-color:#fff;cursor:pointer;text-decoration:none;transition:all .2s ease-in-out}a span{display:inline-block;min-width:9.6rem}a:visited{color:#333}a:hover{color:#000;border-color:#d9d9d9;background-color:#f6f8fa}a+a{margin-top:.6rem}a svg{display:inline-block;width:1.2rem}@media (max-width:768px){body{flex-direction:column;overflow:auto}header{width:100vw;height:auto;padding:5rem 0;align-items:center;padding-right:0}header p{max-width:80vw;text-align:center}.links{width:100vw;height:auto;align-items:center;padding-left:0;background-color:#fff}}
+    *{padding:0;margin:0;font:inherit}body{display:flex;height:100vh;font-family:Inter,sans-serif;overflow:hidden}header{display:flex;flex-direction:column;align-items:flex-end;justify-content:center;width:40vw;height:100vh;padding-right:2.4rem}header svg,header img{width:auto;height:1.8rem}header h1{font-size:1.6rem;font-weight:600;line-height:2;color:#24292e}header p{font-size:1rem;color:#586069;max-width:25rem;text-align:right}.links{display:flex;flex-direction:column;justify-content:center;align-items:flex-start;width:60vw;height:100vh;padding-left:2.4rem;border-left:1px solid #e9e9e9;background-color:#f9f9f9}a{display:flex;align-items:center;gap:.6rem;padding:.5rem 1.2rem;border:1px solid #e1e4e8;border-radius:.5rem;font-size:16px;line-height:1;color:#333;background-color:#fff;cursor:pointer;text-decoration:none;transition:all .2s ease-in-out}a span{display:inline-block;min-width:9.6rem}a:visited{color:#333}a:hover{color:#000;border-color:#d9d9d9;background-color:#f6f8fa}a+a{margin-top:.6rem}a svg{display:inline-block;width:1.2rem}@media (max-width:768px){body{flex-direction:column;overflow:auto}header{width:100vw;height:auto;padding:5rem 0;align-items:center;padding-right:0}header p{max-width:80vw;text-align:center}.links{width:100vw;height:auto;align-items:center;padding-left:0;background-color:#fff}}
   </style>
   <body>
     <header>
